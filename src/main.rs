3a14500c7d8177c8795b25aa84e07abaf729446e
mm-masahiro <hand5.mm9.mzh@gmail.com>
2023-07-17 16:47:03 +0900

    rust thread

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::thread;
+use std::sync::{Arc, Mutex, mpsc};
 
 struct Person {
     name: String,
@@ -83,17 +84,81 @@ fn main() {
     // 参照 = 所有権を別の変数に譲渡するのではなく、値へのアクセスを許す方法。所有権は変わらない
     // 参照は元の所有者のライフタイムよりも長く生きることはできない
 
-    let mut important_data = "Hello, World!!".to_string();
+    // let mut important_data = "Hello, World!!".to_string();
 
-    important_data = calc_data(important_data);
+    // important_data = calc_data(important_data);
 
-    println!("{}", important_data);
+    // println!("{}", important_data);
+   
+    let mut handles = Vec::new();
+
+    // for x in 0..10 {
+    //     handles.push(thread::spawn(move || {
+    //         println!("Hello world in thred: {}", x);
+    //     }))
+    // }
+
+    let data = Arc::new(Mutex::new(vec![1; 10]));
+
+    for x in 0..10 {
+        let data_ref = data.clone();
+        handles.push(thread::spawn(move || {
+            // lockを使ってdataへの可変参照を得る
+            let mut data = data_ref.lock().unwrap();
+            data[x] += 1;
+        }));
+    }
+
+    
+    for handle in handles {
+        let _ = handle.join();
+    }
+
+    dbg!(data);
+
+    message_passing();
 }
 
-fn calc_data(data: String) -> String {
-    println!("{}", data);
-    data
-    thread::spawn(|| {
-        println!("Hello, world!");
-    })
+// fn calc_data(data: String) -> String {
+//     println!("{}", data);
+//     data
+// }
+
+fn message_passing() {
+  let mut handles = Vec::new();
+  let mut data = vec![1; 10];
+  let mut snd_channels = Vec::new();
+  let mut rcv_channels = Vec::new();
+
+  for _ in 0..10 {
+      // mainから各スレッドへのチャンネル
+      let (snd_tx, snd_rx) = mpsc::channel();
+      // 各スレッドからmainへのチャンネル
+      let (rcv_tx, rcv_rx) = mpsc::channel();
+
+      snd_channels.push(snd_tx);
+      rcv_channels.push(rcv_rx);
+
+      handles.push(thread::spawn(move || {
+          let mut data = snd_rx.recv().unwrap();
+          data += 1;
+          let _ = rcv_tx.send(data);
+      }));
+  }
+
+  // 各スレッドにdataの値を送信
+  for x in 0..10 {
+      let _ = snd_channels[x].send(data[x]);
+  }
+
+  // 各スレッドからの結果をdataに格納
+  for x in 0..10 {
+      data[x] = rcv_channels[x].recv().unwrap();
+  }
+  
+  for handle in handles {
+      let _ = handle.join();
+  }
+
+  dbg!(data);
 }
