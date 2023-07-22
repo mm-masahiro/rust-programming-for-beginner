a6120f6f5ea06750f450c814f298c4c4fd4893ee
mm-masahiro <hand5.mm9.mzh@gmail.com>
2023-07-22 16:00:26 +0900

    rust test

--- src/module_a.rs
+++ src/module_a.rs
@@ -1,3 +1,18 @@
 pub fn module_fn() {
     println!("test")
 }
+
+pub fn add(x: i32, y: i32) -> i32 {
+    return x + y;
+}
+
+// #[test]アトリビュートをつけたら、それ以降のものがcargo testで実行できる
+// けど、これだとプロダクションのコードにテストコードが混ざっちゃうから、テストモジュールを作ってそっちでテスト書くとかのほうがいいかも
+#[test]
+fn test_add() {
+    assert_eq!(0, add(0, 0));
+    assert_eq!(1, add(1, 0));
+    assert_eq!(1, add(0, 1));
+    assert_eq!(2, add(1, 1));
+    assert_ne!(3, add(1, 1));
+}
